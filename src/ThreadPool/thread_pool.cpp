f4d26ee41d85b4667f848fdb903fe5f225cd8a00
myanwc <anton.kostyrev@yandex.ru>
2026-02-17 14:59:24 +0300

    Reserve space for threads handles

--- src/ThreadPool/thread_pool.cpp
+++ src/ThreadPool/thread_pool.cpp
@@ -7,6 +7,7 @@
 
 
 ThreadPool::ThreadPool(size_t count) {
+    hThreads_.reserve(count);
     for (size_t i = 0; i < count; ++i) {
         hThreads_.push_back(
             CreateThread(NULL, 0, ThreadStartWrapper, this, 0, NULL)
